406fb2b73ff807e494ee4488d0169457f67ba65f
Your Name <ramifazio@gmail.com>
2023-07-25 12:04:51 -0300

    Add templates and index navigations

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { useSelector } from "react-redux";
-//* Componets
+//* Routes
 import Root from "./routes/Root";
 import ErrorPage from "./routes/ErrorPage";
 import StepOne from "./routes/StepOne";
 import StepTwo from "./routes/StepTwo";
-import StepTree from "./routes/StepTree";
-import Presentacion from "./routes/Presentacion";
 import Login from "./routes/Login";
+//* Components
+import Index from "./components/templates/Index.jsx";
 //* Helpers
 import { handleCheckUser } from "./helpers/index";
+
 //! Root seria la navbar, dentro del "Outlet" se renderizan los children.
 //! Leer docs de react-router-dom para usar loaders y actions dependiendo el proyecto.
 //? https://reactrouter.com/en/main/start/tutorial
@@ -23,10 +24,13 @@ const loggedRouter = createBrowserRouter([
     children: [
       { path: "/", element: <StepOne /> }, //* Elegir tipo de datos
       { path: "/2", element: <StepTwo /> }, //* Elegir tipo de pantalla
-      { path: "/3", element: <StepTree /> }, //* Elegir tema oscuro/claro
     ],
   },
-  { path: "/presentacion", element: <Presentacion />, errorElement: <ErrorPage /> },
+  {
+    path: "/presentacion/:idTemplate",
+    element: <Index />,
+    errorElement: <ErrorPage />,
+  },
 ]);
 
 const notLoggedRouter = createBrowserRouter([
--- src/routes/Presentacion.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Presentacion = () => {
-  return <div>Presentacion</div>;
-};
-
-export default Presentacion;
