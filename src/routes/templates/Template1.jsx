598b3de6fb7afdef7eb3af2308be17b5956b380d
Your Name <ramifazio@gmail.com>
2023-08-11 12:21:13 -0300

    md breakpoint template 1 ready

--- src/routes/templates/Template1.jsx
+++ src/routes/templates/Template1.jsx
@@ -13,14 +13,17 @@ import SwiperComponent from "../../components/Swiper";
 const Template1 = () => {
   return (
     <>
-      <section className="flex h-[8%] w-full items-center justify-between bg-enfasis p-2 font-anton text-3xl text-white shadow-xl sm:justify-center">
-        <img src={logoCij} className="h-full sm:hidden" />
+      <section className="flex h-[8%] w-full items-center bg-enfasis p-2 font-anton text-3xl text-white shadow-xl sm:justify-center md:justify-between">
+        <img src={logoCij} className="hidden h-full md:flex " />
         <span>Operacion X</span>
-        <img src={logoMpf} className="h-full sm:hidden" />
+        <img src={logoMpf} className="hidden h-full md:flex" />
       </section>
       <section id="blank" className="h-[2%]"></section>
-      <main className="grid  gap-2 overflow-y-scroll bg-base p-2 sm:grid-cols-2  md:grid-cols-3 md:gap-4 xl:grid-cols-4 2xl:grid-cols-6">
-        <section className="templateSections enfasisBorders sm:hidden xl:row-span-1 xl:flex 2xl:col-span-1">
+      <main className="grid  gap-2 overflow-x-hidden overflow-y-scroll bg-base p-2 sm:grid-cols-2  md:grid-cols-3 md:gap-4 xl:grid-cols-4 2xl:grid-cols-6">
+        <section className="templateSections enfasisBorders hidden md:row-start-3 md:flex ">
+          <PieChartComponent />
+        </section>
+        <section className="templateSections enfasisBorders sm:hidden md:row-start-3 md:flex xl:row-span-1 xl:flex 2xl:col-span-1">
           <RadarChartComponent />
         </section>
         <section className="templateSections enfasisBorders sm:col-span-2 md:col-span-3 xl:col-span-2 2xl:col-span-2 ">
@@ -32,17 +35,17 @@ const Template1 = () => {
         <section className="templateSections enfasisBorders sm:hidden xl:flex 2xl:col-span-1">
           <PieChartComponent />
         </section>
-        <section className="templateSections enfasisBorders col-span-2  xl:row-span-4 2xl:col-span-1 2xl:row-span-6">
+        <section className="templateSections enfasisBorders col-span-2 md:col-span-1  xl:row-span-4 2xl:col-span-1 2xl:row-span-6">
           <TableComponent />
         </section>
-        <section className="templateSections enfasisBorders sm:col-span-1 sm:row-start-2 md:row-span-2 2xl:col-span-4 2xl:row-span-3">
+        <section className="templateSections enfasisBorders sm:col-span-1 sm:row-start-2 md:col-start-2  2xl:col-span-4 2xl:row-span-3">
           <CircularProgress />
         </section>
 
         <section className="templateSections enfasisBorders hidden 2xl:col-span-4 2xl:col-start-2  2xl:row-span-3 2xl:row-start-5 2xl:flex">
           <PieChartComponent />
         </section>
-        <section className="templateSections enfasisBorders sm:col-start-2 sm:row-start-2 md:hidden xl:row-span-2 xl:flex 2xl:hidden">
+        <section className="templateSections enfasisBorders sm:col-start-2 sm:row-start-2 md:col-start-3 xl:row-span-2 xl:flex 2xl:hidden">
           <GeneralInfo />
         </section>
         <section className="templateSections enfasisBorders p-0 sm:hidden md:row-span-2 xl:row-span-4 2xl:hidden">
@@ -60,7 +63,7 @@ const Template1 = () => {
         <section className="templateSections enfasisBorders sm:col-span-2 sm:row-span-1 md:col-span-3 xl:col-span-2 xl:row-span-2 2xl:col-span-1 2xl:row-span-6">
           <SwiperComponent />
         </section>
-        <section className="templateSections enfasisBorders hidden 2xl:col-span-6 2xl:row-span-1 2xl:flex">
+        <section className="templateSections enfasisBorders hidden  md:row-start-3 md:flex 2xl:col-span-6 2xl:row-span-1 2xl:flex">
           <PieChartComponent />
         </section>
       </main>
