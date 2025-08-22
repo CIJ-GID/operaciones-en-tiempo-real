a4deb95f5532d7c456cb24b3d2e52bf5446208b2
GID <163316811+GIDcij@users.noreply.github.com>
2025-08-22 15:09:45 -0300

    Update index.js
    Actualizadas las fuerzas intervinientes

--- src/assets/fuerzas/index.js
+++ src/assets/fuerzas/index.js
@@ -6,15 +6,31 @@ import logo5 from "./Logo_policia_federal.png";
 import logo6 from "./Logo_policia_salta.png";
 import logo7 from "./Logo_prefectura.png";
 import logo8 from "./Logo_policia_tierradelfuego.png";
-
+import logo9 from "./Logo_policia_bsas.png";
+import logo10 from "./Logo_policia_entrerios.png";
+import logo11 from "./Logo_policia_formosa.png";
+import logo12 from "./Logo_policia_larioja.png";
+import logo13 from "./Logo_policia_misiones.png";
+import logo15 from "./Logo_policia_sanluis.png";
+import logo14 from "./Logo_policia_stafe.png";
+import logo16 from "./Logo_policia_tucuman.png";
+import logo17 from "./Logo_policia_sanjuan.png";
+import logo18 from "./Logo_policia_santacruz.png";
+import logo19 from "./Logo_policia_santiago.png";
 
 export const images = [
-  logo1,
-  logo2,
+  logo4,
+  logo9,
+  logo10,
+  logo11,
+  logo12,
   logo3,
-  logo5,
-  logo2,
-  logo7,
-  logo3,
-  logo5,
+  logo13,
+  logo6,
+  logo14,
+  logo15,
+  logo16,
+  logo17,
+  logo18,
+  logo19
 ];
